c77a82cfe17043a795d7b5d633cdbf90ead61644
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-07-24 18:31:11 -0300

    home screen for buyers - minor layout updates

--- app/src/main/java/com/delivery/kaufen/kaufen/HomeActivity.java
+++ app/src/main/java/com/delivery/kaufen/kaufen/HomeActivity.java
@@ -3,6 +3,8 @@ package com.delivery.kaufen.kaufen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,8 +12,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button logout;
-    FirebaseAuth auth;
+    private Button logout;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +33,12 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_client, menu);
+        return true;
     }
 }
