14f7539a19f66b5de5e6f5ccc3c121bce75cbaf2
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-07-25 15:05:41 -0300

    buyer client home minor updates added new sections

--- app/src/main/java/com/delivery/kaufen/kaufen/HomeActivity.java
+++ app/src/main/java/com/delivery/kaufen/kaufen/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,6 +40,12 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.home_client, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
         return true;
     }
 }
