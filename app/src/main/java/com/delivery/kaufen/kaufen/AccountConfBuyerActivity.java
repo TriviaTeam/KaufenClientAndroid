90da4a68dbd0f43da84f7266906609e150668ccc
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-07-27 15:27:35 -0300

    added section for anything buy

---
+++ app/src/main/java/com/delivery/kaufen/kaufen/AccountConfBuyerActivity.java
@@ -0,0 +1,13 @@
+package com.delivery.kaufen.kaufen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountConfBuyerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_conf_buyer);
+    }
+}
